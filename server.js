140be112adb11de2c31ecddea4a5553e9bb50125
openglory <rr4l813102+github@gmail.com>
2025-02-18 18:44:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,40 +3,27 @@ const WebSocket = require("ws");
 const PORT = process.env.PORT || 10000;
 const server = new WebSocket.Server({ port: PORT });
 
-let clients = new Map();
+let clients = new Set();
 
 server.on("connection", (ws) => {
-    ws.on("message", (message) => {
-        let data = JSON.parse(message);
+    clients.add(ws);
+    console.log("New client connected. Total clients:", clients.size);
 
-        if (data.type === "setName") {
-            if ([...clients.values()].includes(data.name)) {
-                ws.send(JSON.stringify({ type: "nameError" }));
-            } else {
-                clients.set(ws, data.name);
-                updateClients();
-                ws.send(JSON.stringify({ type: "nameConfirmed", name: data.name }));
-            }
-        } else if (data.type === "message") {
-            for (let [client, name] of clients) {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: "message", name: data.name, message: data.message }));
-                }
+    ws.on("message", (message) => {
+        console.log("Received:", message);
+
+        // Broadcast to all clients
+        for (let client of clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
             }
         }
     });
 
     ws.on("close", () => {
         clients.delete(ws);
-        updateClients();
+        console.log("Client disconnected. Total clients:", clients.size);
     });
 });
-
-function updateClients() {
-    const deviceNames = [...clients.values()];
-    for (let client of clients.keys()) {
-        client.send(JSON.stringify({ type: "updateDevices", devices: deviceNames }));
-    }
-}
 
 console.log(`WebSocket server running on port ${PORT}`);
